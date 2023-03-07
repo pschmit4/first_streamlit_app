b05c569628c369f4df96e70e022ae9ed44d8d306
pschmit4 <127094513+pschmit4@users.noreply.github.com>
2023-03-07 15:01:22 +0100

    Update streamlitt_app.py

--- streamlitt_app.py
+++ streamlitt_app.py
@@ -22,6 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#create function
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 # import requests
 try:
@@ -29,13 +35,15 @@ try:
   if not fruit_choice:
     streamlit.error("please select a fruit")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # streamlit.text(fruityvice_response.json())
     # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.write('The user entered ', fruit_choice)
+    # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # streamlit.write('The user entered ', fruit_choice)
     # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    # streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
